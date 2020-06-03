b08d535cc04a9f4af921ed45066e9226d0cd6cba
HiLcc <752194418@qq.com>
2020-06-03 22:50:01 +0800

    细节调整

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # pyqt-camera-example
-使用pyqt和opencv编写的界面程序
+使用pyqt和opencv编写的界面程序。
+第一次写开源程序，有很多不足，还多包涵。
+
+## 环境构建
+项目使用pipenv管理
+
+```
+pip install pipenv
+pipenv install 
+```
+## 软件设计
+需要补全
+1 类图
+2 时序图
--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import sys
 from PyQt5.QtWidgets import QApplication
 
 from mainwindow import MainWindow
-#
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    with open('Aqua.qss', 'r') as f:
+        app.setStyleSheet(f.read())
     w = MainWindow()
     w.show()
     sys.exit(app.exec_())
